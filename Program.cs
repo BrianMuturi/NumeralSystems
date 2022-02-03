541fa3859285aecc17379ecbdb258790e7fa818e
Brian Muturi <muturibrian18@gmail.com>
2022-02-03 19:32:37 +0100

    changes

--- Program.cs
+++ Program.cs
@@ -4,29 +4,54 @@ namespace NumeralSystems
 {
     class Program
     {
+       
         
         static void Main(string[] args)
         {
+            MainProgram();
+
+            void MainProgram()
+            {
+                Console.WriteLine("Type a number:");
+                string input = Console.ReadLine();
+                try
+                {
+                    NumberSystem numberSystem;
+                    if (input[0] == '0' && input[1] != 'x' && input[1] != 'X' && input[1] != ',')   
+                    {
+                        numberSystem = new Octal(input);
+                    }
+                    else if (input[1] == 'x' || input[1] == 'X') 
+                    {
+                        numberSystem = new Hexadecimal(input);
+                    }
+                    else 
+                    {
+                        numberSystem = new Decimal(input);
+                    }
+                    numberSystem.ShowResults();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                LoopOfMainProgram();
+            }
             
+            void LoopOfMainProgram()
+            {
+                while (true)
+                {
+                    Console.WriteLine("Do you want to Convert another number?  Type 'yes' or 'no'");
+                    string answer = Console.ReadLine();
+                    if (answer != "no")
+                    {
+                        if (answer == "yes") { Console.Clear(); MainProgram(); break; }
+                        else Console.WriteLine("Wrong answer");
+                    }
+                    else { Environment.Exit(0); break; }
+                }
+            }
         }
-
-        static void Decimal()
-        {
-            Console.Write("Enter number to convert:");
-            int number = Convert.ToInt32(Console.ReadLine());
-            string binary = Convert.ToString(number, 2);
-
-            Console.Write("In Binary:" + { binary});
-        }
-
-        /*static void Octal()
-        {
-            Console.Write("Enter number to convert:");
-            int number = Convert.ToInt32(Console.ReadLine());
-            string binary = Convert.ToString(number, 2);
-
-            Console.WriteLine(binary);
-        }*/
-
     }
 }
