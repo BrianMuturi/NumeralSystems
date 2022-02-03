7754025939b39a8af1997d44711da25495cf15b0
Brian Muturi <muturibrian18@gmail.com>
2022-02-03 15:00:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NumeralSystems
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void Decimal()
+        {
+            Console.Write("Enter number to convert:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string binary = Convert.ToString(number, 2);
+
+            Console.Write("In Binary:" + { binary});
+        }
+
+        /*static void Octal()
+        {
+            Console.Write("Enter number to convert:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string binary = Convert.ToString(number, 2);
+
+            Console.WriteLine(binary);
+        }*/
+
+    }
+}
